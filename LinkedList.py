583ebe8a8aebccdf3f6056a36e5f648b95e53c29
Adrian Mendoza <68600509+AdrianMendoza07@users.noreply.github.com>
2026-04-08 16:30:09 +0000

    Se arreglo el funcionamiento de deleteByValue

--- LinkedList.py
+++ LinkedList.py
@@ -1,26 +1,26 @@
 
 class LinkedList:
     def __init__(self):
-        self.head = None
-
-    def search(self, target):
-        current = self.head
-
-        while current is not None:
-            if current.data == target:
-                return True
-            current = current.next
-
-        return False  
+        self.head = None  
 
     def deleteByValue(self, value):
         if self.head is None:
             print("Lista vacia")
+            return
         
-        if self.search(value) == True:
+        if self.head.data == value:
             self.head = self.head.next
-        else:
-            print("Value not in List")
+            print(value + " elimnado de la lista")
+            return
+        current = self.head
+        
+        while current is not None:
+            if current.next.data == value:
+                current.head = current.next.next
+                print(value + " elimnado de la lista")
+                return
+            current = current.next
+        print("Valor no esta en la lista")
             
     def size(self):
         current = self.head
